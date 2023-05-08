e4f7dafa4053c9b14c7584d8f22dadb0f8ebb6dd
Ana Alfaro-Garcia <73010333+anaalfarogarciaa@users.noreply.github.com>
2023-05-07 22:51:39 -0700

    Add files via upload

---
+++ Copepath-IOS-My-Story-main/IOS_Project_1_My_Story/IOS_Project_1_My_Story/Context.swift
@@ -0,0 +1,14 @@
+//
+//  Context.swift
+//  IOS_Project_1_My_Story
+//
+//  Created by Ana Alfaro-Garcia on 05/07/2023.
+//
+
+import UIKit
+
+struct Context  {
+    let title: String
+    let image: UIImage
+    let text: String
+}
